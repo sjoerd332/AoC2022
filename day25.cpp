260f82bb5a26a4d577dda0cb4766d532f35f021a
Sjoerd Nijhof <sjoerd.vdbos@hotmail.com>
2022-12-30 19:53:38 +0100

    Generalized big number type

--- day25.cpp
+++ day25.cpp
@@ -4,14 +4,17 @@
 #include <cmath>
 #include <array>
 #include <utility>
+#include <cstdint>
 
 using namespace std;
 
 #define DBG 0
 
-int pow(const int base, const int power)
+typedef int64_t nrType;
+
+nrType pow(const int base, const int power)
 {
-    int val = 1;
+    nrType val = (unsigned long long) 1;
     for(int i = 0; i < power; i++)
     {
         val *= base;
@@ -20,69 +23,68 @@ int pow(const int base, const int power)
 }
 
 // helper voor staartdeling, factorial voor 5 machten
-int f5(const int power)
+nrType f5(const int power)
 {
-    int ans = 0;
+    nrType ans = (unsigned long long) 0;
     for(int i = 0; i <= power; i++)
     {
-        ans += 2*pow(5, i);
+        ans += ((nrType)2)*pow(5, i);
     }
     return ans;
 }
 
 // Alternative: staartdeling achtig
-string intToSnafu3(const int iNumber)
+string intToSnafu(const nrType iNumber)
 {
-    int rest = iNumber;
+    nrType rest = iNumber;
     const array<pair<const int, const char>, 5> values = {
         make_pair(0, '0'),
         make_pair(1, '1'),
         make_pair(2, '2'),
         make_pair(-1, '-'),
         make_pair(-2, '=')}; // order matters for iteration
-    int maxExp = 0;
     int curExp = 0;
-    int nrToSubtract = 0;
+    nrType nrToSubtract = 0;
     string retval = "";
     bool done = false;
+    while(abs(rest) > f5(curExp))
+    {
+        curExp++;
+    }
+    const int maxExp = curExp;
     while(curExp > -1 || done == false)
     {
         nrToSubtract = 0;
-        if(abs(rest) > f5(curExp))
+        bool subtracted  = false;
+        for (const auto& m: values)
         {
-            curExp++;
-        }
-        else
-        {
-            bool subtracted  = false;
-            for (const auto& m: values)
+            const nrType nr = m.first * pow(5, curExp);
+            if(((rest >= 0 && (nr >= rest || m.first == 2)) || (rest < 0 && (nr <= rest || m.first == -2))) && subtracted == false)
             {
-                const int nr = m.first * pow(5, curExp);
-                if(((rest >= 0 && (nr >= rest || m.first == 2)) || (rest < 0 && (nr <= rest || m.first == -2))) && subtracted == false)
-                {
-                    nrToSubtract = nr;
-                    retval += m.second;
-                    subtracted = true;
-                }
+                nrToSubtract = nr;
+                retval += m.second;
+                subtracted = true;
             }
-            rest -= nrToSubtract;
-            curExp--;
-            if(curExp == -1)
-                done = true;
         }
-        if(curExp > maxExp)
-            maxExp = curExp;
+        if(subtracted == false)
+        {
+            retval += values[0].second;
+        }
+        rest -= nrToSubtract;
+        curExp--;
+        if(curExp == -1)
+            done = true;
     }
     if(rest != 0)
-            cout << "Error: rest is not zero: " << rest << endl;
+            cout << endl << "Error: rest is not zero: " << rest << endl << endl;
     if(retval.size() == 0)
         retval = values[0].second;
     return retval;
 }
 
-int SnafuToInt(const string sNumber)
+nrType SnafuToInt(const string sNumber)
 {
-    int val = 0;
+    nrType val = 0;
     int stringPos = sNumber.size()-1;
     int powerPos = 0;
     while(stringPos >= 0)
@@ -123,13 +125,13 @@ int main() {
     #endif
 
     // Part 1
-    int sum = 0;
+    nrType sum = 0;
     for(string line; getline(f, line); )
     {
         sum += SnafuToInt(line);
     }
-    cout << "test: " << SnafuToInt("1=1") << endl;
-    cout << "Answer part 1: " << sum << " (sum), and in SNAFU: " << intToSnafu3(sum) << endl;
+    //cout << "test: " << SnafuToInt("1=1") << endl;
+    cout << "Answer part 1: " << sum << " (sum), and in SNAFU: " << intToSnafu(sum) << endl;
     f.close();
     return 0;
 }
